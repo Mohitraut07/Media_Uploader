d622256fe6a7261a98b28af37dbd2d1db39b2bbc
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-10-16 01:21:10 +0530

    Refactor Navbar component to improve layout and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="w-full h-20 bg-slate-50 flex justify-between items-center gap-4 font-medium">
+    <div className="w-full h-20 bg-slate-50 flex justify-between  items-center md:gap-4  gap-3 font-medium">
       <NavLink to={"/"} className="mx-5 hover:text-xl hover:text-blue-400 transition ease-in-out">MediaUploader</NavLink>
       <div className="flex justify-between items-center gap-x-5 mx-5">
-        <NavLink to={"/login"} className="hover:bg-black hover:text-white p-2 rounded-md active:text-blue-400">Login</NavLink>
-        <NavLink to={"/signup"} className="hover:bg-black hover:text-white p-2 rounded-md active:text-blue-400">SignUp</NavLink>
+        <NavLink to={"/login"} className="hover:bg-black hover:text-white px-5 py-2 rounded-md active:text-blue-400">Login</NavLink>
+        <NavLink to={"/signup"} className="hover:bg-black hover:text-white px-5 py-2 rounded-md active:text-blue-400">SignUp</NavLink>
       </div>
     </div>
   );
